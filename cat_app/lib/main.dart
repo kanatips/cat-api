f85c34fdf273ff75d1cc205e2bd59bfce1370a78
kanatip-admin <kanatip.sam@gmail.com>
2025-04-02 10:01:39 +0700

    aa

--- cat_app/lib/main.dart
+++ cat_app/lib/main.dart
@@ -1,7 +1,9 @@
 
+import 'package:cat_app/app/home.dart';
 import 'package:cat_app/app/login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
@@ -10,16 +12,39 @@ void main() async{
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+// ดึงค่า UID จาก SharedPreferences
+Future<String?> getUserUid() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('uid');
+}
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+     
+//       home: Login(),
+//     );
+//   }
+// }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
-      home: Login(),
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder<String?>(
+        future: getUserUid(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Scaffold(body: Center(child: CircularProgressIndicator())); 
+          } else {
+            return snapshot.hasData ? Home() : Login(); // ถ้ามี UID ไป Home ถ้าไม่มีไป Login
+          }
+        },
+      ),
     );
   }
 }
